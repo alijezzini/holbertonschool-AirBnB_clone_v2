bd4c3f158f3dc4305bbaf51b467ed895587b7606
Ali Jezzini <ali.a.jezzini@gmail.com>
2024-04-19 11:43:00 +0300

    mycommits

--- models/state.py
+++ models/state.py
@@ -1,41 +1,33 @@
 #!/usr/bin/python3
-"""This is the state class"""
-import models
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 from models.city import City
 from os import getenv
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
+import models
+import shlex
 
 
 class State(BaseModel, Base):
-    """This is the class for State
-
-    Attributes:
-        name: input name
-
-    """
-
+    """table of the class State"""
     __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
 
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state',
-                              cascade='all, delete-orphan')
-    else:
-        name = ''
-
-        @property
-        def cities(self):
-            """Returns the list of `City` instances
-            with `state_id` equals to the current
-            """
-
-            cities = list()
-
-            for _id, city in models.storage.all(City).items():
-                if city.state_id == self.id:
-                    cities.append(city)
-
-            return cities
+    @property
+    def cities(self):
+        """return the cities of the current state"""
+        var = models.storage.all()
+        lista = []
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                lista.append(var[key])
+        for elem in lista:
+            if (elem.state_id == self.id):
+                result.append(elem)
+        return (result)
